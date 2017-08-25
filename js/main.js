1a8055a80c4cf6eabd22c5c13b2ed0af3641b26f
Karley Walker <kbw2as@virginia.edu>
2017-08-25 16:35:04 -0400

    remove fill-screen on close

--- js/main.js
+++ js/main.js
@@ -169,6 +169,10 @@
 	function hideContent() {
 		var gridItem = gridItems[current], contentItem = contentItems[current];
 
+		var el = document.getElementById('theGrid')
+
+		removeClass(el, 'fill-screen')
+
 		classie.remove(contentItem, 'content__item--show');
 		classie.remove(contentItemsContainer, 'content--show');
 		classie.remove(closeCtrl, 'close-button--show');
@@ -205,6 +209,15 @@
 		}
 		window.scrollTo(xscroll, yscroll);
 	}
+
+	function removeClass(el, className) {
+  if (el.classList)
+    el.classList.remove(className)
+  else if (hasClass(el, className)) {
+    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+    el.className=el.className.replace(reg, ' ')
+  }
+}
 
 	init();
 
